f0c80f0fb8c775211c5555d4fc60d581fe50211b
Leone Varando <lvarando@gmail.com>
2024-02-23 17:37:40 -0600

    Added metar search

--- client/src/renderer/src/pages/home.jsx
+++ client/src/renderer/src/pages/home.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import axios from 'axios';
-import config from '../../../../../config.json' assert { type: 'json' }
+import config from '../../../../../config.json' assert { type: 'json' };
 
 export default function Home() {
   const [metar, setMetar] = useState('');
-  const getMetar = async () => {
-    const metarInfo = await axios.get(`https://api.checkwx.com/metar/KAUS?x-api-key=${config.apiKey}`);
+
+  const getMetar = async (e) => {
+    e.preventDefault();
+    const code = e.target[0].value;
+    console.log(code);
+    const metarInfo = await axios.get(`https://api.checkwx.com/metar/${code}?x-api-key=${config.apiKey}`);
     setMetar(metarInfo.data.data[0]);
   }
   return (
     <div>
       <h1 id="home-header">METAR Request</h1>
-      <button onClick={() => getMetar()}>Click Me</button>
+      <form onSubmit={getMetar}>
+        <input name="icao" type="text" />
+        <button type='submit'>Search</button>
+      </form>
       <p>{metar}</p>
     </div>
   )
