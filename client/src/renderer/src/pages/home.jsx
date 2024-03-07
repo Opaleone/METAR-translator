dd3337eee4f57736988113f01f203c08ccc2bb33
Leone Varando <lvarando@gmail.com>
2024-03-06 22:55:22 -0600

    Added icaoCodes
    Added a file with various information about all airports in the world including ICAO codes to be able to check user input for correct icao code entry

--- client/src/renderer/src/pages/home.jsx
+++ client/src/renderer/src/pages/home.jsx
@@ -3,49 +3,49 @@ import axios from 'axios';
 import config from '../../../../../config.json' assert { type: 'json' };
 
 export default function Home() {
-  const [metar, setMetar] = useState('');
-  let curMetar;
-  let curMetarRaw;
-  let metarRmkSplit;
+  // const [metar, setMetar] = useState('');
+  // let curMetar;
+  // let curMetarRaw;
+  // let metarRmkSplit;
 
-  const getMetar = async (e) => {
+  // const getMetar = async (e) => {
+  //   e.preventDefault();
+  //   const code = e.target[0].value;
+  //   console.log(code);
+  //   const metarInfo = await axios.get(`https://api.checkwx.com/metar/${code}/decoded?x-api-key=${config.apiKey}`);
+  //   setMetar(metarInfo);
+  // }
+
+  // if (metar) {
+  //   curMetar = metar.data.data[0];
+  //   curMetarRaw = curMetar.raw_text;
+  //   metarRmkSplit = curMetarRaw.split(' RMK ')[1];
+  // }
+
+  const icaoBool = (e) => {
     e.preventDefault();
-    const code = e.target[0].value;
-    console.log(code);
-    const metarInfo = await axios.get(`https://api.checkwx.com/metar/${code}/decoded?x-api-key=${config.apiKey}`);
-    setMetar(metarInfo);
   }
 
-  if (metar) {
-    curMetar = metar.data.data[0];
-    curMetarRaw = curMetar.raw_text;
-    metarRmkSplit = curMetarRaw.split(' RMK ')[1];
+  const filterCodeArr = [];
+
+  const icaoCodeFile = fs.readFileSync('../assets/icaoCodes.txt', e => {
+    console.log(e.message);
+  }).toString();
+
+  const icaoCodeArr = icaoCodeFile.split('\n');
+
+  for (let i = 1; i < icaoCodeArr.length; i++) {
+    const curIcaoCode = icaoCodeArr[i].split(',')[3];
+    filterCodeArr.push(curIcaoCode);
   }
 
   return (
     <div>
       <h1 id="home-header">METAR Request</h1>
-      <form onSubmit={getMetar}>
+      <form onSubmit={icaoBool}>
         <input name="icao" type="text" />
         <button type='submit'>Search</button>
       </form>
-    {curMetar ? 
-      <div>
-        <h1 id="metar-title">METAR Results for {curMetar.icao}</h1>
-        <ul id="metar-info-list">
-          <li>Wind: Heading: {curMetar.wind.degrees}, Speed (KTS): {curMetar.wind.speed_kts}</li>
-          <li>Clouds: {curMetar.clouds.map((cloud) => {
-            <>
-              <p>{cloud.code} clouds at {cloud.feet} feet</p>
-            </>
-          })}</li>
-          <li>Temp: {curMetar.temperature.celsius}° C</li>
-          <li>Dewpoint: {curMetar.dewpoint.celsius}° C</li>
-          <li>Barometer (hg): {curMetar.barometer.hg}</li>
-          <ul>Remarks</ul>
-            <li>{metarRmkSplit}</li>
-        </ul>
-      </div> : <div></div>}
     </div>
   )
 }
